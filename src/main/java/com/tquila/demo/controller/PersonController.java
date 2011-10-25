dfec4ca4cd26c478d8ee004b719553f9d4c661da
Nicola Tassini <nicola@tquila.com>
2011-10-25 22:56:47 +0100

    init

---
+++ src/main/java/com/tquila/demo/controller/PersonController.java
@@ -0,0 +1,63 @@
+package com.tquila.demo.controller;
+
+import java.util.List;
+
+import com.tquila.demo.dao.PersonDao;
+import com.tquila.demo.model.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/person/")
+public class PersonController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
+
+	@Autowired
+	private PersonDao personDao;
+	
+	@RequestMapping(method=RequestMethod.GET,value="edit")
+	public ModelAndView editPerson(@RequestParam(value="id",required=false) Long id) {		
+		logger.debug("Received request to edit person id : "+id);				
+		ModelAndView mav = new ModelAndView();		
+ 		mav.setViewName("edit");
+ 		Person person = null;
+ 		if (id == null) {
+ 			person = new Person();
+ 		} else {
+ 			person = personDao.find(id);
+ 		}
+ 		
+ 		mav.addObject("person", person);
+		return mav;
+		
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="edit") 
+	public String savePerson(@ModelAttribute Person person) {
+		logger.debug("Received postback on person "+person);		
+		personDao.save(person);
+		return "redirect:list";
+		
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,value="list")
+	public ModelAndView listPeople() {
+		logger.debug("Received request to list persons");
+		ModelAndView mav = new ModelAndView();
+		List<Person> people = personDao.getPeople();
+		logger.debug("Person Listing count = "+people.size());
+		mav.addObject("people",people);
+		mav.setViewName("list");
+		return mav;
+		
+	}
+
+}
