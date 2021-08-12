853c3bff1d7c40d12650951c5cba985649915156
khanh9xk <32120316+khanh9xk@users.noreply.github.com>
2021-08-12 14:35:20 +0700

    Update HomeController.java

--- src/main/java/com/template/demo/controller/HomeController.java
+++ src/main/java/com/template/demo/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.beans.factory.annotation.*;
 /**
  * Sample controller for going to the home page with a message
  */
