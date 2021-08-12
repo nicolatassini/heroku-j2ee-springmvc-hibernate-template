d2c3b95f86eb18e02a4cfdc73de7eefe630c7ab9
khanh9xk <32120316+khanh9xk@users.noreply.github.com>
2021-08-12 14:31:02 +0700

    Add files via upload

---
+++ src/main/java/com/template/demo/model/Category.java
@@ -0,0 +1,92 @@
+package com.template.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+
+/**
+ * Entity to map the User object
+ * 
+ * @author nicolatassini
+ *
+ */
+@Entity
+public class Category extends BaseEntity implements Serializable {
+
+	private static final long serialVersionUID = 5344160974873744816L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+	@Column(length=10000)
+    private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Category{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+
+	/**
+     *
+     */
+    public Category() {
+    }
+
+	/**
+	 * @see Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/**
+	 * @see Object#equals(Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+
+
+}
