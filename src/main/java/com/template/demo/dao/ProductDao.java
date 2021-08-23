7e02d5d8727293e1372831c76043f20f86ddc5d2
khanh9xk <32120316+khanh9xk@users.noreply.github.com>
2021-08-23 15:47:14 +0700

    Update ProductDao.java

--- src/main/java/com/template/demo/dao/ProductDao.java
+++ src/main/java/com/template/demo/dao/ProductDao.java
@@ -1,6 +1,6 @@
 package com.template.demo.dao;
 
-import com.template.demo.model.Product;
+import com.template.demo.model.*;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,11 +25,24 @@ public class ProductDao {
 	
 	@SuppressWarnings("unchecked")
 	public List<Product> getList(String name, Integer categoryId, Integer brandId) {
-		return entityManager.createQuery("select c from Product c where 1=1 AND (:name = '' OR lower(c.name) like lower(concat('%', :name,'%'))) AND (:categoryId = -1 OR c.categoryId = :categoryId) AND (:brandId = -1 OR c.brandId = :brandId)")
+		
+		List<Product> productList = entityManager.createQuery("select c from Product c where 1=1 AND (:name = '' OR lower(c.name) like lower(concat('%', :name,'%'))) AND (:categoryId = -1 OR c.categoryId = :categoryId) AND (:brandId = -1 OR c.brandId = :brandId)")
 			.setParameter("name", name)
 			.setParameter("categoryId", categoryId)
 			.setParameter("brandId", brandId)
 			.getResultList();
+		
+		for(Product p: productList){
+			if(null != p.getBrandId()){
+				Brand brand = entityManager.find(Brand.class, p.getBrandId());
+				p.setBrand(brand.getName());
+			}
+			if(null != p.getCategoryId()){
+				Category category = entityManager.find(Category.class, p.getCategoryId());
+				p.setCategory(category.getName());
+			}
+		}
+		return productList;
 	}
 	
 	@Transactional
