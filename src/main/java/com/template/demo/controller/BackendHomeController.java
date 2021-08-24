e49c5a8a1dbacede47004af2dc445a0094a9d3a5
khanh9xk <32120316+khanh9xk@users.noreply.github.com>
2021-08-24 13:55:06 +0700

    Update BackendHomeController.java

--- src/main/java/com/template/demo/controller/BackendHomeController.java
+++ src/main/java/com/template/demo/controller/BackendHomeController.java
@@ -211,7 +211,7 @@ public class BackendHomeController extends BaseController implements Serializabl
 	 */
 	@RequestMapping(value = "/api/product", method = RequestMethod.PUT, consumes="application/json", produces = "application/json")
 	@ResponseBody
-	public String updateProduct(@RequestBody ProductUpdateDTO productDto ) {
+	public String updateProduct(@RequestBody Product productDto ) {
 		logger.info("====> [Backend] Brand update: " + productDto.toString());
 		productDao.save(productDto);
 		return "success";
