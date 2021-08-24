f1a2b0aa56dd3a6a8ef3ffc093ece2fcfb5fafdf
khanh9xk <32120316+khanh9xk@users.noreply.github.com>
2021-08-24 11:08:56 +0700

    Add files via upload

---
+++ src/main/java/com/template/demo/dto/ProductUpdateDTO.java
@@ -0,0 +1,80 @@
+package com.template.demo.dto;
+
+public class ProductUpdateDTO  {
+
+    private Integer id;
+
+    private String name;
+
+	private String description;
+
+	private Integer categoryId;
+
+	private Integer brandId;
+	
+	private String image;
+	
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public Integer getBrandId() {
+		return brandId;
+	}
+
+	public void setBrandId(Integer brandId) {
+		this.brandId = brandId;
+	}
+}
